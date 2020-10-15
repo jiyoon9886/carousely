8d39a9a7d7b5064e8411a91b6281607f57b7eb75
Jiyoon Kim <jiyoon9886@gmail.com>
2020-10-15 00:03:20 -0400

    created MyCarousel component

---
+++ src/MyCarousel.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Carousel } from 'antd';
+import ReactPlayer from 'react-player';
+import './carouselStyles.css';
+
+export default function MyCarousel() {
+  const contentStyle = {
+    height: '300px',
+    color: '#fff',
+    textAlign: 'center',
+  };
+
+  function onChange(a, b, c) {
+    console.log(a, b, c);
+  }
+
+  return (
+    <div>
+      <Carousel afterChange={onChange} effect='fade'>
+        <div className='player-wrapper'>
+          <ReactPlayer
+            className='react-player'
+            style={contentStyle}
+            url='https://www.youtube.com/watch?v=G9Lk9-A44OY'
+            width='100%'
+            height='100%'
+          />
+        </div>
+        <div className='player-wrapper'>
+          <ReactPlayer
+            className='react-player'
+            style={contentStyle}
+            url='https://www.youtube.com/watch?v=DndhLYgjVzw'
+            width='100%'
+            height='100%'
+          />
+        </div>
+        <div className='player-wrapper'>
+          <ReactPlayer
+            className='react-player'
+            style={contentStyle}
+            url='https://www.youtube.com/watch?v=gvLihpGqfXQ'
+            width='100%'
+            height='100%'
+          />
+        </div>
+        <div className='player-wrapper'>
+          <ReactPlayer
+            className='react-player'
+            style={contentStyle}
+            url='https://www.youtube.com/watch?v=VIgwPAiV4ZE'
+            width='100%'
+            height='100%'
+          />
+        </div>
+      </Carousel>
+    </div>
+  );
+}
