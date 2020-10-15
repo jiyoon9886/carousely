b2c0340543cce8ff2bab1c50ba903cf86cac1f03
Jiyoon Kim <jiyoon9886@gmail.com>
2020-10-15 17:27:44 -0400

    imported UpArrow to App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 import MyCarousel from './MyCarousel';
+import UpArrow from './UpArrow';
 
 function App() {
   return (
     <div className='App'>
       <MyCarousel />
+      <UpArrow />
     </div>
   );
 }
